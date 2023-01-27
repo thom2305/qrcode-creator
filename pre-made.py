2727378bfcbdf726887fc69f40b167bb4a350375
thom2305 <102167733+thom2305@users.noreply.github.com>
2023-01-27 18:02:04 +0000

    Done

--- pre-made.py
+++ pre-made.py
@@ -1,12 +1,21 @@
-
-# Importing library
 import qrcode
- 
-# Data to be encoded
-data = 'QR Code using make() function'
- 
-# Encoding data using make() function
-img = qrcode.make(data)
- 
-# Saving as an image file
-img.save('MyQRCode1.png')
+
+def printqr(ele,name):
+    data = ele
+    img = qrcode.make(data)
+    img.save(name + '.png')
+
+def question():
+    awns = input('Do You Want To Restart Yes Or No:')
+    if awns == 'yes':
+        qrmaker()
+    if awns == 'no': 
+        print('Goodbye!')
+        exit()
+def qrmaker():
+    nm = input('What would be in the qr code:')
+    nmfl = input('What would be the name of the file:')
+    printqr(nm,nmfl)
+    question()
+
+qrmaker()
